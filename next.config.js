511514f6e3f9fe8e28bd3368de7ab40eba06e6d9
juliuscoros <julius@glyph.com.ph>
2023-09-26 20:07:16 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    env: {
+        baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+        spotify: {
+            baseUrl: process.env.NEXT_PUBLIC_SPOTIFY_BASE_URL,
+            clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET
+        },
+        test: 'hatdawg'
+    }
+}
 module.exports = nextConfig
